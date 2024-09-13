ea014484ddda3378aa2d85dc6bdc711e179915f6
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-09-13 21:01:51 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240913/Date to Date/date-to-date.cpp
+++ 240913/Date to Date/date-to-date.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int m1, m2;
     int d1, d2;
-    int elapsedTime = 0;
+    int elapsedTime = 1;
     int days[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     cin >> m1 >> d1 >> m2 >> d2;
@@ -21,7 +21,7 @@ int main()
         if(d1 > days[m1])
         {
             m1++;
-            d1 = 0;
+            d1 = 1;
         }        
     }
     cout << elapsedTime;
