c5f781533bc24d4023f11b839ffc339d29f8ca70
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-09-15 18:35:29 +0900

    Title: 겹치지 않는 문자 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240915/겹치지 않는 문자 2/non-overlapping-characters-2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int board[1002][3];
+int d[1002][3];   //a :0, b: 1, c: 2
+int first[1002];
+
+int main() 
+{
+    int n;
+    int a, b, c;
+
+    cin >> n; //길이가 n임
+
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> board[i][0] >> board[i][1] >> board[i][2]; 
+    }
+
+    d[1][0] = board[1][0]; //첫번째 a
+    d[1][1] = board[1][1]; //첫번째 b
+    d[1][2] = board[1][2]; //첫번째 c
+    
+    //처음에 뭐 골랐는지    
+    if(d[1][1] < d[1][2])
+        first[0] = 1;
+    else
+        first[0] = 2;
+    
+    if(d[1][0] < d[1][2])
+        first[1] = 0;
+    else
+        first[1] = 2;
+
+    if(d[1][0] < d[1][1])
+        first[2] = 0;
+    else
+        first[2] = 1;
+
+    for(int i = 2; i <= n; ++i)
+    {
+        //현재 선택하는 것이 board[i][k] 임,        
+        d[i][0] = min(d[i-1][1], d[i-1][2]) + board[i][0];
+        d[i][1] = min(d[i-1][0], d[i-1][2]) + board[i][1];
+        d[i][2] = min(d[i-1][0], d[i-1][1]) + board[i][2];
+    }
+
+    cout << *min_element(d[n], d[n]+3);
+    
+
+    return 0;
+}
