a02791b9e0660a4df4f353aa8ced031903d3294b
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-09-18 22:46:43 +0900

    Title: 수열 조작, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240918/수열 조작/sequence-manipulation.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+deque<int>dq;
+
+int main() 
+{
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; ++i)
+    {
+        dq.push_back(i+1);
+    }
+
+    while(1)
+    {
+        if(dq.size() == 1)
+        {
+            cout <<dq.back();
+            break;
+        }
+
+        dq.pop_front();
+        int temp = dq.front();
+        cout << temp <<"\n";
+        dq.pop_front();
+        dq.push_back(temp);
+    }
+
+    return 0;
+}
