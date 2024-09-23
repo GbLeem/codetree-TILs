2b545f3cc1e7ea166a2989bdc2fb2d62823fd99d
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-09-23 16:48:33 +0900

    Title: 블럭쌓는 명령, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240923/블럭쌓는 명령/block-stacking-commands.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int board[1000002];
+
+int main() 
+{
+    int n, k;
+    cin >> n >> k;
+
+    for(int i = 0; i < k; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        for(int j = a; j <= b; ++j)
+        {
+            board[j]++;
+        }
+    }
+
+    sort(board+1, board+n+1);
+
+    cout << board[n/2];
+
+    return 0;
+}
