f989043f047c3ccfa80b812aa5250f724bc2c144
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-09-30 02:57:24 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int board[25];
+
+int Sum(int a, int b, int c)
+{  
+    int div = 10;
+    int sum = 0;
+
+    while(1)
+    {
+        int temp = 0;
+        int check = 0;
+        
+        temp = a % div + b % div + c % div;
+        check = (a % div) / (div/10) + (b % div) / (div/10) + (c % div) / (div/10);
+        sum += temp;
+        
+        if(check > 10)
+        {        
+            return -1;
+        }
+
+        a = a - a % div;
+        b = b - b % div;
+        c = c - c % div;
+
+        if(a == 0 && b == 0 && c == 0)
+            break;
+
+        div *= 10;
+    }    
+    
+    return sum;
+}
+
+int main() 
+{
+    int n;
+    int maxValue = 0;
+    cin >> n;
+    
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> board[i];
+    }
+
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = i + 1; j < n; ++j)
+        {
+            for(int k = j + 1; k < n; ++k)
+            {
+                if(Sum(board[i], board[j], board[k]) == -1)
+                    continue;
+                maxValue = max(Sum(board[i], board[j], board[k]), maxValue);
+            }
+        }
+    }
+    cout << maxValue;
+
+    return 0;
+}
