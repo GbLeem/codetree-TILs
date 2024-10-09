128ef494709b761847fb83723191d408a8d653cf
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-09 23:59:37 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241009/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void pr()
+{
+    cout << "**********\n";
+}
+int main() 
+{
+    for(int i = 0; i < 5; ++i)
+    {
+        pr();
+    }
+    return 0;
+}
