ecb7736e7339e14d9d1c7ce18cdc522cf390b9cd
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-10 20:34:02 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 241010/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -31,7 +31,7 @@ int main()
     board[cur.first][cur.second] = dist;
     dist++;
 
-    for (int i = 0; i < n * n - 1; i++)
+    for (int i = 0; i < n * n; i++)
     {
         for (int j = 0; j < 2; ++j)
         {
