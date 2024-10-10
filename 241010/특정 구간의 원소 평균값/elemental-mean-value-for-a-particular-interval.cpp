839032c3abbe436cffa8a0653280195d6b46d990
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-10 17:37:27 +0900

    Title: 특정 구간의 원소 평균값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 241010/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -18,27 +18,24 @@ int main()
     //n이 4인 경우
     for(int i = 0; i < n; ++i) // 시작점 0 ~ 3
     {
-        for(int j = i + 1; j <= n; ++j) // 몇 개를 고를지 1 ~ 4 개
+        for(int j = i; j < n; ++j) // 몇 개를 고를지 1 ~ 4 개
         {
             int tempSum = 0;
             int count = 0;
-            int size = i + j;
-
-            if(size > n) //2 + 4 > 4
-                size = n;
-            for(int k = i; k < size; ++k)
+        
+            for(int k = i; k <= j; ++k)
             {
-                tempSum += board[k];
-                count++;
+                tempSum += board[k];                
             }
-            double avg = (double)tempSum / count;
+            double avg = (double)tempSum / (j - i + 1);
 
-            for(int k = i; k < size; ++k)
+            for(int k = i; k <= j; ++k)
             {
                 if(board[k] == avg)
                 {
-                    answer++;                    
-                }
+                    answer++;                              
+                    break;
+                }        
             }
         } 
     }
