f44581cd84e0328173b56574d3539a2afae715eb
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-10 15:52:37 +0900

    Title: G or H 3, Time: 31ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/G or H 3/G-or-H-3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+char board[102];
+
+int main() 
+{
+    int n, k;
+    cin >> n >> k;
+
+    int maxNum = 0;
+    for(int i = 0; i < n; ++i)
+    {
+        int num;
+        char ch;
+
+        cin >> num >> ch;
+        
+        if(maxNum < num)
+            maxNum = num;
+
+        board[num] = ch;
+    }    
+
+    int answer = 0;
+
+    for(int i = 1; i <= maxNum - k; ++i)
+    {
+        int temp = 0;
+        for(int j = i; j <= i + k; ++j) // 1 ~ 7
+        {
+            if(board[j] == 'G')
+                temp += 1;
+            else if(board[j] == 'H')
+                temp += 2;
+        }
+        answer = max(answer, temp);
+    }
+    
+    cout << answer;
+
+    return 0;
+}
