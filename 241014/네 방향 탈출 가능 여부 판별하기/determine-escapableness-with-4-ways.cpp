98903b008d0430690582e253d5048a6ea1ba1e32
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-14 15:20:14 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int board[102][102];
+int vis[102][102];
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+
+queue<pair<int, int>> Q;
+int n, m;
+
+void BFS()
+{
+    while(!Q.empty())
+    {
+        auto cur = Q.front();
+        Q.pop();
+
+        for(int dir = 0; dir < 4; ++dir)
+        {
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+
+            if(board[nx][ny] == 0 || vis[nx][ny] == 1)
+                continue;
+            if(nx < 0 || nx > n || ny < 0 || ny > m)
+                continue;
+            Q.push({nx, ny});
+            vis[nx][ny] = 1;
+        }
+    }
+}
+
+int main() 
+{
+    cin >> n >> m;
+
+    for(int i = 0; i < n; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            cin >> board[i][j];
+        }
+    }
+
+    Q.push({0,0});
+    vis[0][0] = 1;
+
+    BFS();    
+
+    cout << vis[n - 1][m - 1];
+    
+    return 0;   
+}
