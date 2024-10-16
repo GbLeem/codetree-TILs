16d58eac4f830f54d0b49ec0572800d503d44c21
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-16 15:09:34 +0900

    Title: 행복한 수열의 개수, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 241016/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 241016/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int board[102][102];
 
-int main() 
+int main()
 {
     int n, m;
     cin >> n >> m;
     int answer = 0;
 
-    for(int i =0 ; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        for(int j = 0; j < n; ++j)
+        for (int j = 0; j < n; ++j)
         {
             cin >> board[i][j];
         }
@@ -20,41 +20,49 @@ int main()
     //연속하여 m개 이상의 동일한 원소 등장
 
     //x증가
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         int temp = board[i][0]; //시작점
         int cnt = 1;
+        int maxCnt = 0;
         //y증가
-        for(int j = 1; j < n; ++j)
+        for (int j = 1; j < n; ++j)
         {
-            if(temp != board[i][j])
+            if (temp != board[i][j])
             {
+                maxCnt = max(maxCnt, cnt);
                 temp = board[i][j];
                 cnt = 1;
             }
-            else 
+            else
                 cnt++;
         }
-        if(cnt >= m)
+        maxCnt = max(maxCnt, cnt);
+        if (maxCnt >= m)
             answer++;
     }
 
-    for(int i = 0; i < n; ++i)
+    //y증가
+    for (int i = 0; i < n; ++i)
     {
         int temp = board[0][i]; //시작점
         int cnt = 1;
-        //y증가
-        for(int j = 1; j < n; ++j)
+        int maxCnt = 0;
+
+        //x증가
+        for (int j = 1; j < n; ++j)
         {
-            if(temp != board[j][i])
+            if (temp != board[j][i])
             {
+                maxCnt = max(maxCnt, cnt);
                 temp = board[j][i];
                 cnt = 1;
             }
-            else 
+            else
                 cnt++;
         }
-        if(cnt >= m)
+        maxCnt = max(maxCnt, cnt);
+        if (maxCnt >= m)
             answer++;
     }
 
