1b88ffcd290337a6bd2237293c1f10d03564751e
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-17 20:55:21 +0900

    Title: 돌 잘 치우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241017/돌 잘 치우기/clear-stones-well.cpp
+++ 241017/돌 잘 치우기/clear-stones-well.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
 int n, k, m; //격자, 시작점 수, 치워야 할 돌
 int board[102][102];
+int tempBoard[102][102];
 int vis[102][102];
+vector<pair<int,int>> start;
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+
+int answer = 0;
 
 int BFS(int x, int y)
 {
@@ -54,6 +59,31 @@ int BFS(int x, int y)
     return cnt;
 }
 
+void Choose(int cur)
+{        
+    //m개 다 지운 경우
+    if(cur == m + 1)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            answer = max(answer, BFS(start[i].first, start[i].second));
+        }
+    }
+
+    //n*n보다 작은 숫자
+    for(int i = 0; i < n * n; ++i)
+    {
+        int x = i / n;
+        int y = i % n;
+
+        //돌인 경우 (1인 경우)
+        if(board[x][y] == 1)
+        {
+            board[x][y] = 0;
+            Choose(cur + 1);
+        }
+    }
+}
 int main() 
 {
     cin >> n >> k >> m;
@@ -63,20 +93,20 @@ int main()
         for(int j = 0; j < n; ++j)
         {
             cin >> board[i][j];
+            tempBoard[i][j] = board[i][j];
         }
     }
-
-    int gocnt = 0;
-
+    
     for(int i = 0; i < k; ++i)
     {
-        int r, c;
-        cin >> r >> c;     
-
-        //BFS하기    
-        gocnt += BFS(r, c);
+        int x, y;
+        cin >> x>> y;
+        start.push_back({x, y});
     }
 
-    cout << gocnt;
+    Choose(1);
+
+    cout << answer;
+
     return 0;
 }
