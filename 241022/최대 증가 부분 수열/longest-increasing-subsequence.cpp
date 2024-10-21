be441970a9dec46aa105968935ebf5c12aa65789
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-22 01:34:09 +0900

    Title: 최대 증가 부분 수열, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241022/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 241022/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -10,28 +10,26 @@ int main()
 {
     cin >> n;
 
-    for(int i = 0; i < n; ++i)
+    for(int i = 1; i <= n; ++i)
     {
         cin >> board[i];
         d[i] = INT_MIN;
     }
 
-    //가장 긴 증가 부분 수열의 길이
-    d[0] = 0;
+    d[1] = 1;
+    //가장 긴 증가 부분 수열의 길이    
     //i에 도착했을 때 가장 긴 증가 수열의 길이
-    for(int i = 1; i < n; ++i)
+    for(int i = 2; i <= n; ++i)
     {
-        for(int j = 0; j < i; ++j)
-        {
-            if(d[j] == INT_MIN)
-                continue;
-            if(j + board[j] >= i)
+        for(int j = 1; j < i; ++j)
+        {            
+            if(board[i] > board[j])
                 d[i] = max(d[i], d[j] + 1);
         }
     }
 
     int ans = 0;
-    for(int i = 0; i < n; ++i)
+    for(int i = 1; i <= n; ++i)
         ans = max(ans, d[i]);
 
     cout << ans;
