e3c77105964ef7e4e166e5460a98a091f159a882
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-22 16:28:00 +0900

    Title: 2차원 최대 증가 수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241022/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int board[55][55];
+int d[55][55];
+//현재보다 큰 수로 이동
+//최소 x+1, y+1 한 위치
+
+void Print()
+{
+    for(int i = 1; i <= n; ++i)
+    {
+        for(int j = 1; j <= m; ++j)
+        {
+            cout << d[i][j] <<" ";
+        }
+        cout <<"\n";
+    }
+}
+
+int main() 
+{
+    cin >> n >> m;
+
+    for(int i = 1; i <= n; ++i)
+    {
+        for(int j = 1; j <= m; ++j)
+        {
+            cin >> board[i][j];
+        }
+    }
+
+    //i j 위치에 왔을때 최대 칸의 수
+    d[1][1] = 1;
+
+    for(int i = 1; i <= n; ++i)
+        d[i][1] = 1;
+    for(int i = 1; i <= m; ++i)
+        d[1][i] = 1;
+
+
+    for(int i = 2; i <= n; ++i)
+    {
+        for(int j = 2; j <= m; ++j)
+        {
+            for(int k = 1; k < i; ++k)
+            {
+                for(int l = 1; l < j; ++l)
+                {
+                    if(board[i][j] > board[k][l] && i > k && j > l)
+                    {
+                        d[i][j] = max(d[i][j], d[i-k][j-l] + 1);
+                    }
+                }
+
+            }
+        }
+    }
+
+    Print();
+
+    int answer = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+        for(int j = 1; j <= m; ++j)
+        {  
+            answer = max(answer, d[i][j]);
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
