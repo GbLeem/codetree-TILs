1c7d44fb63bf67601c79f4bfa59a25019136a6da
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-24 17:40:46 +0900

    Title: 1차원 폭발 게임, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241024/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int board[102];
+int Bsize = 0;
+bool change = false;
+
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> board[i];
+        Bsize++;
+    }
+
+    while (1)
+    {
+        change = false;
+        int cnt = 1;
+        int idx = 0;
+        bool first = true;
+
+        for (int i = 0; i < Bsize; ++i)
+        {
+            //같은 것 발견
+            if (board[i] == board[i + 1])
+            {
+                cnt++;
+                if (first)
+                {
+                    idx = i;
+                    first = false;
+                }
+            }
+            else
+            {
+                //터지는 경우
+                if (cnt >= m)
+                {
+                    change = true;
+                    int temp[102]; //복사용 배열
+                    int tempIdx = 0; //새로운 배열 크기
+
+                    for (int j = 0; j < Bsize; ++j)
+                    {
+                        if (idx <= j && j < idx + cnt) //1 <= j < 3
+                            continue;
+                        temp[tempIdx] = board[j];
+                        tempIdx++;
+                    }
+                    //size 갱신
+                    Bsize = tempIdx;
+
+                    //board로 copy
+                    for (int j = 0; j < Bsize; ++j)
+                    {
+                        board[j] = temp[j];
+                    }
+                    cnt = 1;
+                    idx = 0;
+                    //change = false;
+                    first = false;
+                    break;
+                }
+            }
+        }
+
+        if (!change)
+            break;
+    }
+
+    cout << Bsize << "\n";
+    for (int i = 0; i < Bsize; ++i)
+    {
+        cout << board[i] << "\n";
+    }
+
+
+    return 0;
+}
