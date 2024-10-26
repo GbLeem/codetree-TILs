19da52db2099e589ed3fea99a5d6da60e6eea4b2
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-26 14:21:41 +0900

    Title: 가장 먼저 나오는 숫자, Time: 214ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241026/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> board;
+
+int main() 
+{
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i)
+    {
+        int num;
+        cin >> num;
+
+        board.push_back(num);
+    }
+
+    for(int i = 0; i < m; ++i)
+    {
+        int num;
+        cin >> num;
+
+        if(lower_bound(board.begin(), board.end(), num) == upper_bound(board.begin(), board.end(), num))
+            cout << -1<< "\n";
+        else
+            cout << lower_bound(board.begin(), board.end(), num) - board.begin() + 1 << "\n";
+    }
+
+    return 0;
+}
