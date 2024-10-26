210b150d9c32131d137b30ad9c86657932cdaeab
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-26 15:50:42 +0900

    Title: 삼 오 무, Time: 1091ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241026/삼 오 무/three-five-moo.cpp
+++ 241026/삼 오 무/three-five-moo.cpp
@@ -2,22 +2,46 @@
 using namespace std;
 
 int n;
-long long M = 1'000'000'000;
+int M = 1'000'000'000;
+
+bool Count(int k)
+{
+    int temp = n;
+    for(int i = 1; i <= k; ++i)
+    {
+        if(i % 3 != 0 && i % 5 != 0)
+            temp--;
+
+        if(temp == 0)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
 
 int main() 
 {
     cin >> n;
     
-    for(int i = 1; i <= M; ++i)
-    {
-        if(i % 3 != 0 && i % 5 != 0)
-            n--;
-        if(n == 0)
-        {
-            cout << i;
-            break;
-        }
-    }
+    int left = 1;
+    int right = M;
+    int answer = M;
 
+    while(left <= right)
+    {
+        int mid = (left + right) / 2;
+
+        if(Count(mid))
+        {
+            right = mid - 1;
+            answer = min(answer, mid);
+        }
+        else
+            left = mid + 1;
+    } 
+            
+    cout << answer;
     return 0;
 }
