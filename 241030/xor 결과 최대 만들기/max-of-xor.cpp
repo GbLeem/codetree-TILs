8875cacbcc64113ef6a5289ef8d7a2149a9cbcbc
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-30 02:21:25 +0900

    Title: xor 결과 최대 만들기, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

--- 241030/xor 결과 최대 만들기/max-of-xor.cpp
+++ 241030/xor 결과 최대 만들기/max-of-xor.cpp
@@ -1,63 +1,45 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int n, m;
 int board[22];
-vector<int> ans;
-bool isused[1'000'002];
+int vis[22];
 int answer = 0;
 
 int Check()
 {
-    int temp = ans[0];
-    if(ans.size() == 1)
-        return temp;
-    for(int i = 1; i < ans.size(); ++i)
-    {
-        temp = temp^ans[i];
-    }
-
-    return temp;
-}
-
-void Print()
-{
-    for(auto a : ans)
-    {
-        cout << a << " ";
-    }
-    cout <<"\n";
-}
-
-void Choose(int cur)
-{
-    if(cur == m + 1)
-    {
-        answer = max(answer, Check());        
-        return;
-    }
+    int ans = 0;
     for(int i = 0; i < n; ++i)
     {
-        if(!isused[board[i]])
-        {
-            ans.push_back(board[i]);
-            isused[board[i]] = true;
-            Choose(cur + 1);
-            ans.pop_back();
-            isused[board[i]] = false;
-        }
+        if(vis[i])
+            ans ^= board[i];        
     }
+    return ans;
 }
+
+void Choose(int cur, int cnt)
+{
+    if(cnt == m)
+    {
+        answer = max(answer, Check());
+        return;
+    }
+    if(cur == n)
+        return;
+
+    Choose(cur + 1, cnt);
+    vis[cur] = true;
+    Choose(cur + 1, cnt + 1);
+    vis[cur] = false;
+}
+
 int main() 
 {
     cin >> n >> m;
+
     for(int i = 0; i < n; ++i)
         cin >> board[i];
-
-    Choose(1);
-    cout << answer;    
-
-   
+    Choose(0,0);
+    cout << answer;
     return 0;
 }
