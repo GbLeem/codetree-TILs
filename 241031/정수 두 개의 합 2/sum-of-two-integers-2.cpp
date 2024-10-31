72ea00ee3e014ba1b0b046b2a286ccad9f556348
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-10-31 20:46:41 +0900

    Title: 정수 두 개의 합 2, Time: 1094ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241031/정수 두 개의 합 2/sum-of-two-integers-2.cpp
+++ 241031/정수 두 개의 합 2/sum-of-two-integers-2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int n, k;
@@ -12,6 +13,8 @@ int main()
     for(int i = 0; i < n; ++i)
         cin >> board[i];
 
+    sort(board, board + n);
+
     int en = 0;
     int cnt = 0;
     int sum = 0;
@@ -19,30 +22,21 @@ int main()
     for(int st = 0; st < n; ++st)
     {
         en = st;
-        while(en < n) //끝까지 안가고 / 0개 1개 선택 / 합이 k보다 작을 때
-        {            
-            //새로운 것 선택
+        while(en < n && sum + board[en] <= k)
+        {
             sum += board[en];
             cnt++;
 
-            //그만 선택
-            if(cnt == 2 && sum <= k)
+            if(cnt == 2)
             {
-                answer++;      
-                sum -= board[en];                
-                cnt--;
-            }    
-
-            else if(sum > k || cnt > 2)
-            {
+                answer++;
                 sum -= board[en];
                 cnt--;
-            }
-
-            en++;            
-        }        
-        sum = 0;
-        cnt = 0;    
+            }   
+            en++;
+        }
+        sum -= board[st];
+        cnt = 0;
     }
 
     cout << answer;
