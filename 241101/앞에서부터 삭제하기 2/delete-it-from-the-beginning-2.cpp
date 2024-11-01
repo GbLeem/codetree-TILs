bb3c0a592e81d5027e7f86ce1da4400a73688257
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-01 15:37:01 +0900

    Title: 앞에서부터 삭제하기 2, Time: 18ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 241101/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 241101/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int n;
 int board[100'002];
-priority_queue<int> pq;
+priority_queue<int, vector<int>, greater<int>> pq;
 float answer = 0;
+int minValue = 0;
+int sum = 0;
 
 int main() 
 {
@@ -16,29 +18,26 @@ int main()
     for(int i = 0; i < n; ++i)
     {
         cin >> board[i]; // 3 1 9 2 7 
+        pq.push(board[i]);
+        sum += board[i];
     }
     
-    //k : 1 ~ n-2    
+    minValue = pq.top(); //가장 작은 값    
+    int cnt = n - 1;
+
+    //k : 1 ~ n-2
     for(int k = 1; k <= n - 2; ++k)
-    {               
-        priority_queue<int> tempPQ;
-        
-        for(int i = k; i < n; ++i)
-        {
-            tempPQ.push(board[i]);    
-        }
+    {   
+        int removeValue = board[k - 1]; // 없앨 값      
 
-        float tempValue = 0;
-        int cnt = 0;
+        if(removeValue < minValue)      // 가장 작은 값 리셋    
+            minValue = removeValue;
+                    
+        sum -= removeValue; //없애기
+        sum -= minValue;        
+        cnt--;
 
-        while(tempPQ.size() > 1)
-        {
-            tempValue += tempPQ.top();
-            tempPQ.pop();
-            cnt++;
-        }
-        //cout << (float)tempValue / cnt << "\n";
-        answer = max(answer, tempValue / cnt);
+        answer = max(answer, (float)sum / cnt);
     }
     
     cout << fixed;
