be7d46e6fc5213dbcfe70c9038dba12c935f6a1c
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-02 01:03:47 +0900

    Title: 0에 가장 가까운 합, Time: 41ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241102/0에 가장 가까운 합/sum-closest-to-zero.cpp
+++ 241102/0에 가장 가까운 합/sum-closest-to-zero.cpp
@@ -8,32 +8,44 @@ int n;
 vector<int> board;
 int answer = INT_MAX;
 
-int main() 
+//테스트케이스 5번
+int main()
 {
     cin >> n;
-    for(int i = 0; i < n ; ++i)
+    for (int i = 0; i < n; ++i)
     {
         int num;
         cin >> num;
         board.push_back(num);
     }
 
-    sort(board.begin(), board.end());
+    sort(board.begin(), board.end()); //-123 1 1 2 2 124
 
     int en = n - 1;
-    int minValue = INT_MAX;
 
     for (int st = 0; st < n; ++st)
     {
-        if(st < en)
-            answer = min(answer, abs(board[st] + board[en]));
-
-        while(st < en - 1 && board[st] + board[en] > 0)
+        int minValue = INT_MAX;
+        while (en != 0 && abs(board[en] + board[st]) < minValue)
         {
+            minValue = abs(board[en] + board[st]);
+
             en--;
-            answer = min(answer, abs(board[st] + board[en]));
-        }   
-    }    
+            answer = min(answer, minValue);
+
+            if (answer == 0)
+            {
+                cout << 0;
+                return 0;
+            }
+            //조건을 만족하지 못했는데, 모든 경우 다 봤다면
+            if (en <= st)
+                break;
+        }
+
+        if (en <= st)
+            break;
+    }
 
     cout << answer;
     return 0;
