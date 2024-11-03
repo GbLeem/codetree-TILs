9f52218000f3e20795c4ef99f5f6e9c619ea6ad0
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-03 17:11:25 +0900

    Title: 점 개수 세기 3, Time: 164ms, Memory: 1MB, Status: Passed - CodeTree

--- 241103/점 개수 세기 3/count-number-of-points-3.cpp
+++ 241103/점 개수 세기 3/count-number-of-points-3.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <set>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
 
 int n, q;
 int board[100'002];
-set<int> s;
+//set<int> s;
 unordered_map<int, int> um;
 
 int main() 
@@ -13,20 +13,19 @@ int main()
     cin >> n >> q;
     for(int i = 0; i < n; ++i)
     {
-        cin >> board[i];
-        s.insert(board[i]);
-    }        
-
-    set<int>::iterator it;
-
+        cin >> board[i];        
+    }
+    sort(board, board + n);
+    
     int cnt = 1;
-    for(it = s.begin(); it != s.end(); ++it)
+
+    for(int i = 0; i < n; ++i)
     {
-        um[*it] = cnt;
-        //cout << *it << " -> " << cnt<<"\n";
-        board[*it] = cnt;
+        um[board[i]] = cnt;    //um[-3] = 1
+        board[i] = cnt;       //board[0] = 1;
         cnt++;
     }    
+    
     // for(int i = 0; i < n; ++i)
     //     cout << board[i] <<" ";
         
