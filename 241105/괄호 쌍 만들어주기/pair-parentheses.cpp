66f893883cad29ad451cbd058caab27414e793c1
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-05 19:54:20 +0900

    Title: 괄호 쌍 만들어주기, Time: 16ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241105/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int board[10'002];
+
+int main() 
+{
+    string str;
+    cin >> str;
+
+    int sSize = str.size();
+
+    for(int i = sSize; i >= 0; --i)
+    {
+        if(str[i] == ')' && str[i + 1] == ')')
+        {
+            board[i] = board[i+1] + 1;
+        }
+        else
+            board[i] = board[i+1];
+    }
+
+    long long answer = 0;
+    for(int i = 0; i < sSize; ++i)
+    {
+        if(str[i] == '(' && str[i+1] == '(')
+            answer += board[i+2];
+    }
+
+    cout << answer;
+    return 0;
+}
