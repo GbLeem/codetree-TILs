b43ccef4a4020ac0023fe272875a4e7988e9eff3
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-11 18:24:48 +0900

    Title: 화재 진압, Time: 21ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241111/화재 진압/fire-suppression.cpp
+++ 241111/화재 진압/fire-suppression.cpp
@@ -6,39 +6,42 @@ int n, m;
 int fire[100'002];
 int office[100'002];
 int answer = 0;
-int main() 
+
+int main()
 {
     cin >> n >> m;
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         cin >> fire[i]; //가능성
     }
 
-    for(int i = 0; i < m; ++i)
+    for (int i = 0; i < m; ++i)
     {
         cin >> office[i];
     }
 
     int en = 0;
     int temp = INT_MAX;
-    
+
     //office
-    for(int st = 0; st < m; ++st)
-    {   
+    for (int st = 0; st < m; ++st)
+    {
         //fire
-        if(st == m - 1)
+        while (en < n)
         {
-            while(en < n)
+            if (st < m - 1)
             {
-                answer = max(answer, abs(office[st]-fire[en]));
-                en++;
+                if (abs(office[st] - fire[en]) <= abs(office[st + 1] - fire[en]))
+                {
+                    answer = max(answer, abs(office[st] - fire[en]));
+                    en++;
+                }
+                else
+                    break;
             }
-        }
-        else
-        {
-            while(en < n && abs(office[st] - fire[en]) <= abs(office[st + 1] - fire[en]))
+            else
             {
-                answer = max(answer, office[en] - fire[st]);
+                answer = max(answer, abs(office[st] - fire[en]));
                 en++;
             }
         }
