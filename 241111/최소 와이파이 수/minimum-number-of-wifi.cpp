e98a45c6ac239da1b0f48f4008606f547db869dd
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-11 22:53:41 +0900

    Title: 최소 와이파이 수, Time: 1084ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 241111/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 241111/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -13,23 +13,18 @@ int main()
     {
         cin >> board[i]; // 1 1 0 1 1 1
     }
-
+    
     int cur = 1;
-    int cnt = 0;
-    while(cur <= n)
+    while(cur < n)
     {
         if(board[cur] == 1)
         {
-            cnt++;
-        }        
-        if(board[cur] == 0)
-        {
-            answer += (int)(ceil((double)cnt / (2 * m + 1)));
-            cnt = 0;
+            answer++;
+            cur = cur + 2 * m + 1;
         }
-        cur++;
+        if(cur > n)
+            break;
     }
-    answer += (int)(ceil((double)cnt / (2 * m + 1)));
     cout << answer;
     return 0;
 }
