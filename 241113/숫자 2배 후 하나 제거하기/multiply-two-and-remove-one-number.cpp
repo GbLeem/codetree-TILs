c4505385beee45403aec293e2b8a54274fb04e1a
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-13 20:54:18 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 241113/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
+++ 241113/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.cpp
@@ -15,27 +15,28 @@ int main()
 
     for (int i = 0; i < n; ++i)  //두배할 것 설정
     {
-        int temp[102] = {};
-        int idx = 0;
+        board[i] *= 2;
 
         for (int j = 0; j < n; ++j) //제외할 것 설정
         {
-            int tempSum = 0;
+            int temp[102] = {};
+            int idx = 0;
+
             for (int k = 0; k < n; ++k)
-            {
+            {                
                 if (j == k)
                     continue;                
-                if(i == k)
-                    temp[idx++] = board[k]*2;
-                else
-                    temp[idx++] = board[k];
+                temp[idx++] = board[k];
             }
+
+            int tempSum = 0;
             for (int k = 0; k < n - 2; ++k)
             {
                 tempSum += abs(temp[k] - temp[k + 1]);
             }
             answer = min(answer, tempSum);
         }
+        board[i] /= 2;
     }
     cout << answer;
     return 0;
