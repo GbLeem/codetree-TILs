354d95b5b947b8cdae5f1213487a3cb471473a06
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-14 22:44:42 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/두 선분/two-lines.cpp
+++ 241114/두 선분/two-lines.cpp
@@ -3,13 +3,23 @@ using namespace std;
 
 int main() 
 {
-    int x1, x2, x3, x4;
+    int x1, x2, x3, x4;    
     cin >> x1 >> x2 >> x3 >> x4;
 
-    if(x2 >= x3)
-        cout <<"intersecting\n";
-    else
-        cout <<"nonintersecting";
+    if(x1 < x3)
+    {
+        if(x2 >= x3)
+            cout <<"intersecting\n";
+        else
+            cout <<"nonintersecting";
 
+    }
+    else
+    {
+        if(x4 >= x1)
+            cout <<"intersecting\n";
+        else
+            cout <<"nonintersecting";
+    }
     return 0;
 }
