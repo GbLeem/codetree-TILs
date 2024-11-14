41b2b0669d97f21b2c71804bec83823323928406
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-15 02:08:45 +0900

    Title: k번째로 작은 쌍의 합, Time: 451ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

--- 241115/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
+++ 241115/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
@@ -8,51 +8,46 @@ int n, m, k;
 
 vector<int> vec1;
 vector<int> vec2;
-priority_queue<int> pq3;
+priority_queue<int, vector<int>, greater<int>> pq3;
+priority_queue<int> pq;
 
-int main()
+int main() 
 {
     cin >> n >> m >> k;
 
-    for (int i = 0; i < n; ++i)
+    for(int i = 0; i < n; ++i)
     {
         int num;
         cin >> num;
         vec1.push_back(num);
     }
 
-    for (int i = 0; i < m; ++i)
+    for(int i = 0; i < m; ++i)
     {
         int num;
         cin >> num;
         vec2.push_back(num);
     }
 
-    sort(vec1.begin(), vec1.end(), greater<int>()); // 7 6 3 2 1
-    sort(vec2.begin(), vec2.end(), greater<int>()); // 8 7 5 1
+    sort(vec1.begin(), vec1.end());
+    sort(vec2.begin(), vec2.end());
 
-    for (int i = 0; i < vec1.size(); ++i)
+    for(int i = 0; i < vec1.size(); ++i)
     {
-        for (int j = 0; j < vec2.size(); ++j)
+        int j = 0;
+        for(j = 0; j < vec2.size(); ++j)
         {
             pq3.push(vec1[i] + vec2[j]);
+            pq.push(vec1[i] + vec2[j]);
         }
-        if (pq3.size() > 2 * k)
-        {
-            while (pq3.size() > 2 * k)
-            {
-                pq3.pop();
-            }
-        }
-    }
-
-    int size = pq3.size();
-    size -= k;
-    while (size > 0)
+        if(!pq.empty() && pq.top() < vec1[i] + vec2[j])
+            break;
+    }    
+    while(k > 1)
     {
         //cout << pq3.top()<<"\n";
         pq3.pop();
-        size--;
+        k--;
     }
     cout << pq3.top();
     return 0;
