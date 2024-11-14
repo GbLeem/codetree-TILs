f93bf6b90be6f06a5c2665c35cf0f81f497688fd
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-14 17:16:03 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 241114/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <climits>
 using namespace std;
 
 int n;
@@ -11,26 +12,36 @@ int main()
     cin >> n;
     cin >> str;
 
-    int dist = 0;
-
-    for (int i = 0; i < str.size(); ++i)
+    for (int i = 0; i < str.size(); ++i) //새로 들어갈 자리
     {
-        if (dist == 0 && str[i] == '1')
+        if(str[i] == '1')
+            continue;
+        else //0인 경우
         {
-            dist = 1;
-        }
-        else if (i == str.size() - 1)
-        {
-            answer = max(answer, dist);
-        }
-        else if (dist != 0 && str[i] == '0')
-        {
-            dist++;
-        }
-        else if (dist != 0 && str[i] == '1')
-        {
-            answer = max(answer, dist / 2);
-            dist = 1;
+            str[i] = '1';
+            int dist = 0;
+            int minDist = INT_MAX;
+
+            for(int j = 0; j < str.size(); ++j)
+            {
+                //맨 처음
+                if (dist == 0 && str[j] == '1')
+                {
+                    dist = 1;
+                }                
+                else if (dist != 0 && str[j] == '0')
+                {
+                    dist++;
+                }
+                else if (dist != 0 && str[j] == '1')
+                {
+                    minDist = min(minDist, dist);
+                    dist = 1;
+                }
+            }
+            //다시 돌려주기
+            str[i] = '0';
+            answer = max(answer, minDist);
         }
     }
 
