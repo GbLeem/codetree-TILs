6a3471a179cb25084507b98992568a8aa49f0ee4
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-14 15:48:26 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 241114/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -3,34 +3,6 @@ using namespace std;
 
 int A, B, C;
 int answer = 0;
-// int main() 
-// {
-//     cin >> A >> B >> C;
-
-//     int cntA = 0;
-
-//     while(1)
-//     {
-//         int cntB = 0;
-//         int temp = A * cntA + B * cntB;
-//         while(1)
-//         {
-//             temp = A * cntA + B * cntB;
-//             if(temp <= C)
-//                 answer = max(answer, temp);
-//             if(temp > C)
-//                 break;
-//             cntB++;
-//         }   
-//         cntA++;
-
-//         if(cntA * A > C)
-//             break;
-//     }
-
-//     cout << answer;
-//     return 0;
-// }
 
 int main()
 {
@@ -38,7 +10,7 @@ int main()
 
     for(int i = 0; i * A <= C; ++i)
     {
-        int temp = a * i; //A곱한 값
+        int temp = A * i; //A곱한 값
 
         int cnt = (C - temp) / B; //가능한 최대 B 갯수
 
