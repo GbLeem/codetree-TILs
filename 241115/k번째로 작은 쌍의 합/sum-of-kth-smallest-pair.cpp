f409c2f58ec1d9256b251db2b378a873775c8379
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-15 01:31:42 +0900

    Title: k번째로 작은 쌍의 합, Time: 399ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241115/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m, k;
+
+vector<int> vec1;
+vector<int> vec2;
+priority_queue<int, vector<int>, greater<int>> pq3;
+
+void Print1(const vector<int>& vec)
+{
+    for(auto v : vec)
+    {
+        cout << v << " ";
+    }
+    cout << "\n";
+}
+
+int main() 
+{
+    cin >> n >> m >> k;
+
+    for(int i = 0; i < n; ++i)
+    {
+        int num;
+        cin >> num;
+        vec1.push_back(num);
+    }
+
+    for(int i = 0; i < m; ++i)
+    {
+        int num;
+        cin >> num;
+        vec2.push_back(num);
+    }
+    sort(vec1.begin(), vec1.end());
+    sort(vec2.begin(), vec2.end());
+
+    for(int i = 0; i < vec1.size(); ++i)
+    {
+        for(int j = 0; j < vec2.size(); ++j)
+        {
+            pq3.push(vec1[i] + vec2[j]);
+        }
+    }    
+    while(k > 1)
+    {
+        //cout << pq3.top()<<"\n";
+        pq3.pop();
+        k--;
+    }
+    cout << pq3.top();
+    return 0;
+}
