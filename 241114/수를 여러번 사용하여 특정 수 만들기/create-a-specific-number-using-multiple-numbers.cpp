be7cb82a086a67e5121a1264bb022b1020afc163
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-14 15:47:47 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241114/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 241114/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -3,31 +3,49 @@ using namespace std;
 
 int A, B, C;
 int answer = 0;
-int main() 
+// int main() 
+// {
+//     cin >> A >> B >> C;
+
+//     int cntA = 0;
+
+//     while(1)
+//     {
+//         int cntB = 0;
+//         int temp = A * cntA + B * cntB;
+//         while(1)
+//         {
+//             temp = A * cntA + B * cntB;
+//             if(temp <= C)
+//                 answer = max(answer, temp);
+//             if(temp > C)
+//                 break;
+//             cntB++;
+//         }   
+//         cntA++;
+
+//         if(cntA * A > C)
+//             break;
+//     }
+
+//     cout << answer;
+//     return 0;
+// }
+
+int main()
 {
     cin >> A >> B >> C;
 
-    int cntA = 0;
-
-    while(1)
+    for(int i = 0; i * A <= C; ++i)
     {
-        int cntB = 0;
-        int temp = A * cntA + B * cntB;
-        while(1)
-        {
-            temp = A * cntA + B * cntB;
-            if(temp <= C)
-                answer = max(answer, temp);
-            if(temp > C)
-                break;
-            cntB++;
-        }   
-        cntA++;
+        int temp = a * i; //A곱한 값
 
-        if(cntA * A > C)
-            break;
+        int cnt = (C - temp) / B; //가능한 최대 B 갯수
+
+        temp += cnt * B; //최종 값
+
+        answer = max(answer, temp);
     }
 
     cout << answer;
-    return 0;
 }
