cb1f6e060d3c2801811f7d2c72215c8a4101c8ca
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-14 23:45:40 +0900

    Title: 양쪽 모두 존재하는 구간, Time: 987ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241114/양쪽 모두 존재하는 구간/both-exist-section.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n, m;
+int board[100'002];
+int nums[100'002];
+int numsOut[100'002];
+int answer = INT_MAX;
+
+bool Check()
+{
+    for (int i = 1; i <= m; ++i)
+    {
+        if (nums[i] < 1)
+            return false;
+    }
+    return true;
+}
+
+bool Check2()
+{
+    for (int i = 1; i <= m; ++i)
+    {
+        if (numsOut[i] < 1)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> board[i];
+        numsOut[board[i]]++;
+    }
+
+    int en = 0;
+    nums[board[0]] = 1;
+    numsOut[board[0]]--;
+
+    for (int st = 0; st < n; ++st)
+    {
+        while (en < n && en >= st)
+        {
+            if (Check())
+            {
+                if(Check2())                
+                    answer = min(answer, en - st + 1);
+                else
+                    break;
+            }
+            en++;
+            nums[board[en]]++;
+            numsOut[board[en]]--;
+        }
+
+        nums[board[st]]--;
+        numsOut[board[st]]++;
+    }
+    if (answer == INT_MAX)
+        cout << -1;
+    else
+        cout << answer;
+    return 0;
+}
