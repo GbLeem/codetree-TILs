c1fdafa839dbc6107adbb629fbed88618d80a707
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-15 21:51:31 +0900

    Title: 0에 가장 가까운 합 3, Time: 54ms, Memory: 0MB, Status: Passed - CodeTree

--- 241115/0에 가장 가까운 합 3/sum-closest-to-zero-3.cpp
+++ 241115/0에 가장 가까운 합 3/sum-closest-to-zero-3.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int n;
 int board[100'002];
-int answer = INT_MAX;
 int realAnswer = 0;
 
 int main()
@@ -18,28 +17,34 @@ int main()
     sort(board, board + n);
 
     int en = n - 1;
-
+    int minValue = INT_MAX;
+    
     for (int st = 0; st < n; ++st)
     {
-        while (en - 1 > st)
+        //현재 값보다 다음 값이 작으면 en을 내려줌
+        while(en > 1 && abs(board[en - 1] + board[st]) <= abs(board[en] + board[st]))
         {
-            int dist = abs(board[en] + board[st]);
-            if (dist > abs(board[en - 1] + board[st]))
-                en--;
+            en--;
+
+            if(minValue == abs(board[en] + board[st]))            
+                realAnswer = min(realAnswer, board[en] + board[st]);            
             else
+            {
+                if (minValue > abs(board[en] + board[st]))
+                {
+                    minValue = abs(board[en] + board[st]);
+                    realAnswer = board[en] + board[st];
+                }
+            }      
+            
+            if(en == st)
                 break;
         }
-        if (en > st)
+        //아닌 경우 st올리기
+        if (minValue > abs(board[en] + board[st]))
         {
-            if (answer > abs(board[en] + board[st]))
-            {
-                answer = abs(board[en] + board[st]);
-                realAnswer = board[en] + board[st];
-            }
-            else if (answer == abs(board[en] + board[st]))
-            {
-                realAnswer = min(realAnswer, board[en] + board[st]);
-            }
+            minValue = abs(board[en] + board[st]);
+            realAnswer = board[en] + board[st];
         }
     }
 
