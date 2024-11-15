55f349d78b8f1ca251f33c56bb1ca4b6aef9e972
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-16 02:00:16 +0900

    Title: 1이 k개 이상 존재하는 부분 수열, Time: 31ms, Memory: 1MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241116/1이 k개 이상 존재하는 부분 수열/subsequence-with-k-or-more-1s.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n, k;
+int board[1'000'002];
+int answer = INT_MAX;
+int main()
+{
+    cin >> n >> k;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> board[i];
+    }
+
+    int one = 0;
+    int two = 0;
+    int en = 0;
+
+    if (board[0] == 1)
+        one++;
+    else
+        two++;
+
+    for (int st = 0; st < n; ++st)    //1을 가리킨다.
+    {        
+        while (en + 1 > st && one < k)
+        {
+            en++;
+            if (board[en] == 1)
+                one++;
+            else
+                two++;            
+        }
+        answer = min(answer, en - st + 1);
+        if (board[st] == 1)
+            one--;
+        else
+            two--;
+    }
+
+    if (answer == INT_MAX)
+        cout << -1;
+    else
+        cout << answer;
+    return 0;
+}
