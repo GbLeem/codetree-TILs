b52acb9b73dde4a29ce84e70b1ae4dc3a4a0c98b
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-15 17:23:08 +0900

    Title: 고인 물, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241115/고인 물/standing-water.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int n, m;
+stack<int> s;
+int answer = 0;
+
+int main() 
+{
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i)
+    {
+        int h;
+        cin >> h;
+
+        if(s.empty())
+            s.push(h);
+        else
+        {
+            if(s.top() > h)
+            {
+                answer += (s.top() - h);                
+            }
+            else if(s.top() <= h)
+            {
+                s.pop();
+                s.push(h);
+            }
+        }
+    }
+    cout << answer;
+    return 0;
+}
