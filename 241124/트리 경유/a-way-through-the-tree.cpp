c28df9a47577f5b55da0ae2e747aec5858d0176f
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-11-24 18:04:16 +0900

    Title: 트리 경유, Time: 16ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241124/트리 경유/a-way-through-the-tree.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int n, q;
+int vis[200'002];
+
+int main() 
+{
+    cin >> n >> q;
+
+    for(int i = 1; i <= q; ++i)
+    {
+        int dest;
+        cin >> dest;
+        int temp = dest;
+        
+        bool fail = false;
+        
+        //루트 까지 가기
+        while(temp != 1)
+        {   
+            temp /= 2;
+
+            //올라가다가 못가는 곳 만남
+            if(vis[temp])
+            {
+                cout << temp <<"\n";
+                fail = true;
+                break;    
+            }            
+        }   
+        if(!fail)
+        {
+            cout << 0 << "\n";
+            vis[dest] = 1;
+        }
+    }
+
+
+
+    return 0;
+}
