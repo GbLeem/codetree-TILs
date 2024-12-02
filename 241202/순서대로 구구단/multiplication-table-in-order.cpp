20bf0662a1e35b817ac65884f41bcdc0a388289b
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-02 19:22:26 +0900

    Title: 순서대로 구구단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241202/순서대로 구구단/multiplication-table-in-order.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int a, b;
+
+int main() 
+{
+    cin >> a >> b;
+
+    if(a > b)
+    {
+        for(int j = 1; j <= 9; ++j)
+        {
+            for(int i = a; i <= b; --i)
+            {
+                cout << i <<" * " << j << " = "<<i*j<<"  ";
+            }
+            cout << "\n";
+        }
+    }    
+    else
+    {
+        for(int j = 1; j <= 9; ++j)
+        {
+            for(int i = a; i <= b; ++i)
+            {
+                cout << i <<" * " << j << " = "<<i*j<<"  ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
