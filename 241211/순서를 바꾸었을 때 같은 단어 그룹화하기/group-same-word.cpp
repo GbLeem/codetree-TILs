b290471938ad479d531bf94fd197b3c3d49f4ae7
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-11 13:44:31 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 71ms, Memory: 0MB, Status: Passed - CodeTree

--- 241211/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
+++ 241211/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
@@ -7,35 +7,24 @@ using namespace std;
 int n;
 unordered_map<string, int> um;
 
-int main() 
+int main()
 {
     cin >> n;
 
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         string str;
         cin >> str;
 
-        char board[1002];
-        for(int j = 0; j < str.size(); ++j)
-        {
-            board[j] = str[j];
-        }
-        sort(board, board + str.size());
-
-        string temp;
-        for(int j = 0; j < str.size(); ++j)
-        {
-            temp += board[j];
-        }        
-        um[temp]++;
+        sort(str.begin(), str.end());
+        um[str]++;
     }
-    
+
     int answer = 0;
-    for(auto it = um.begin(); it != um.end(); ++it)
+    for (auto it = um.begin(); it != um.end(); ++it)
     {
         answer = max(answer, it->second);
     }
-    cout<<answer;
+    cout << answer;
     return 0;
 }
