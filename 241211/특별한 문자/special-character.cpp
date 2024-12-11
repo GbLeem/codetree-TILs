7677e1eb4de283ecc248614823fd6a55f0b40ccd
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-11 14:29:59 +0900

    Title: 특별한 문자, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241211/특별한 문자/special-character.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<char, int> um;
+
+
+int main() 
+{   
+    string str;
+    cin >> str;
+
+    for(int i = 0; i < str.size(); ++i)
+    {
+        um[str[i]]++;
+    }
+
+    for(int i = 0; i < str.size(); ++i)
+    {
+        if(um[str[i]] == 1)
+        {
+            cout << str[i];
+            return 0;
+        }
+    }
+
+    cout << "None";
+    return 0;
+}
