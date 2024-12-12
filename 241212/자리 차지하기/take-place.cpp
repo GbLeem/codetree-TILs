bd20c14841e124fe9e60d432c13c42b115857817
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-12 15:40:38 +0900

    Title: 자리 차지하기, Time: 53ms, Memory: 6MB, Status: Wrong Answer - CodeTree

--- 241212/자리 차지하기/take-place.cpp
+++ 241212/자리 차지하기/take-place.cpp
@@ -18,49 +18,28 @@ int main()
     for(int i = 1; i <= m; ++i)
     {
         um[i] = 1;
+        s.insert(i);
     }
     // 1 2 3 4
     for(int i = 0; i < n; ++i)
     {
-        //idx  1 2 3
-        //a    4 1 1
-        //idx 사람은 1번 이상 a번 이하의 의자에만 앉는다.
         int num;
         cin >> num;
-        s.insert(num);
         vec.push_back(num);
     }
 
     sort(vec.begin(), vec.end()); //1 1 4
+
     // 1 1 4
     for(int i = 0; i < vec.size(); ++i)
     {
-        for(int j = 1; j <= vec[i]; ++j)
+        if(*s.begin() <= vec[i])
         {
-            if(um[j] == 1)
-            {
-                answer++;
-                um[j]--;
-                break;
-            }
+            s.erase(*s.begin());
+            answer++;
         }
     }
     cout << answer;
-    // for(auto it = s.begin(); it != s.end(); ++it)
-    // {
-    //     //4라면
-    //     cout <<*it <<"\n";
-    //     for(int i = 1; i <= *it; ++i)
-    //     {
-    //         if(um[i] == 1)
-    //         {
-    //             answer++;
-    //             um[i]--;
-    //             break;
-    //         }
-    //     }
-    // }
-
-    //cout << answer;
+        
     return 0;
 }
