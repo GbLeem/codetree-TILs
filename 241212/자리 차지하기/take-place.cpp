41ba9b9986fab8eacc5d158a7f1c9b3e1f95ee31
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-12 16:53:00 +0900

    Title: 자리 차지하기, Time: 74ms, Memory: 5MB, Status: Passed - CodeTree

--- 241212/자리 차지하기/take-place.cpp
+++ 241212/자리 차지하기/take-place.cpp
@@ -1,66 +1,55 @@
 #include <iostream>
 #include <set>
-#include <unordered_map>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int n, m;
 set<int> s;
-vector<int> vec;
+int board[100'002];
 int answer = 0;
 
 int main()
 {
     cin >> n >> m;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> board[i];
+    }
 
     for (int i = 1; i <= m; ++i)
     {
-        s.insert(i); // 1 2 3 4
+        s.insert(i); //1 2 3 4 5
     }
 
     for (int i = 0; i < n; ++i)
     {
-        int num;
-        cin >> num;
-        vec.push_back(num); //4 1 1
-    }
+        auto it = s.lower_bound(board[i]);
 
-    for (int i = 0; i < vec.size(); ++i)
-    {
-        if (!s.empty())
+        if (it != s.end())
         {
-            //자신보다 같거나 작은 값 없애기
-            auto it = s.lower_bound(vec[i]);
-
-            if (it == s.end() || *it > vec[i])
-            {
-                it--;
-            }
-            if (it == s.begin())
-            {
-                if (*it <= vec[i])
-                {
-                    s.erase(*it);
-                    answer++;
-                    cout << answer;
-                    return 0;
-                }                    
-                else
-                {
-                    cout << answer;
-                    return 0;
-                }
-            }
-            if (*it <= vec[i])
+            if (*it == board[i])
             {
                 s.erase(*it);
                 answer++;
             }
+            else
+            {
+                if (it != s.begin())
+                {
+                    --it;
+                    s.erase(*it);
+                    answer++;
+                }
+                else
+                    break;
+            }
+        }
+        else
+        {
+            it--;
+            s.erase(*it);
+            answer++;
         }
     }
-
     cout << answer;
-
     return 0;
 }
