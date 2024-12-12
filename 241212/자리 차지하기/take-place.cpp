a72455cc37f4370225c963e32cb1862bf6e3e195
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-12 16:28:49 +0900

    Title: 자리 차지하기, Time: 1017ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 241212/자리 차지하기/take-place.cpp
+++ 241212/자리 차지하기/take-place.cpp
@@ -10,37 +10,53 @@ set<int> s;
 vector<int> vec;
 int answer = 0;
 
-int main() 
+int main()
 {
     cin >> n >> m;
 
-    for(int i = 1; i <= m; ++i)
+    for (int i = 1; i <= m; ++i)
     {
         s.insert(i); // 1 2 3 4
-    }    
+    }
 
-    for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         int num;
         cin >> num;
         vec.push_back(num); //4 1 1
     }
 
-    for(int i = 0; i < vec.size(); ++i)
+    for (int i = 0; i < vec.size(); ++i)
     {
-        if(!s.empty())
+        if (!s.empty())
         {
-            if(*s.begin() <= vec[i])
+            //자신보다 같거나 작은 값 없애기
+            auto it = s.lower_bound(vec[i]);
+            while (1)
             {
-                s.erase(vec[i]);
-                answer++;
+                if (it == s.end())
+                {
+                    it--;
+                }
+                if (it == s.begin())
+                {
+                    if (*it <= vec[i])
+                        break;
+                    else
+                    {
+                        cout << answer;
+                        return 0;
+                    }
+                }
+                if (*it <= vec[i])
+                    break;
             }
-            else
-                break;
+            s.erase(*it);
+            answer++;
         }
     }
 
     cout << answer;
-        
+
     return 0;
 }
