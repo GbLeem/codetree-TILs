3b2890652ba4f886a2e81ec4c7303383193c92f2
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2024-12-13 16:56:25 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241213/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 241213/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -5,38 +5,38 @@ using namespace std;
 int board[102];
 int answer = INT_MAX;
 
-int main() 
+int main()
 {
     //최소 t번 이상 h높이가 되도록
     int n, h, t;
     cin >> n >> h >> t;
-    
-    for(int i = 0; i < n; ++i)
+
+    for (int i = 0; i < n; ++i)
     {
         cin >> board[i];
-    }    
-    
-    for(int i = 0; i < n - t + 1; ++i)
+    }
+
+    for (int i = 0; i < n - t + 1; ++i)
     {
         int cnt = 0;
         int temp = 0;
-        
-        for(int j = i; j < n; ++j)
+
+        for (int j = i; j < n; ++j)
         {
             //차이
             int cost = abs(h - board[j]);
             //비용 
             temp += cost;
             //연속 횟수 체크
-            cnt--;
-            
-            if(cnt == t)
+            cnt++;
+
+            if (cnt == t)
                 break;
         }
 
         answer = min(answer, temp);
     }
 
-    cout <<answer;
+    cout << answer;
     return 0;
 }
