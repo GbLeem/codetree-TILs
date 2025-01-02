79bb2b69cd87aa1a40d77d17fcc20bcad6aefc9f
LeemHyungJun <86725870+GbLeem@users.noreply.github.com>
2025-01-02 12:37:36 +0900

    Title: 1차원 젠가, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250102/1차원 젠가/jenga-1d.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int blocks[100];
+int s1, e1;
+int s2, e2;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> blocks[i];
+    }
+    cin >> s1 >> e1;
+    cin >> s2 >> e2;
+
+    // Write your code here!
+
+    return 0;
+}
